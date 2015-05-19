3172eae236d7067b9e52e387cb133a176bec52d3
Matt Bentley <mbentley@mbentley.net>
2015-05-19 09:43:24 -0400

    Optimized Dockerfile; formatted run.sh

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,18 @@
 FROM debian:jessie
 MAINTAINER Matt Bentley <mbentley@mbentley.net>
-RUN (echo "deb http://http.debian.net/debian/ jessie main contrib non-free" > /etc/apt/sources.list && echo "deb http://http.debian.net/debian/ jessie-updates main contrib non-free" >> /etc/apt/sources.list && echo "deb http://security.debian.org/ jessie/updates main contrib non-free" >> /etc/apt/sources.list)
-RUN apt-get update
 
 # install typical php packages and then additional packages
-RUN (DEBIAN_FRONTEND=noninteractive apt-get install -y php5-curl php5-gd php5-fpm php5-imagick php5-mcrypt php5-memcache php5-memcached php5-mysql &&\
-	DEBIAN_FRONTEND=noninteractive apt-get install -y dnsutils imagemagick ssmtp wget whois)
-
-# enable new relic agent
-RUN (wget -O - http://download.newrelic.com/548C16BF.gpg | apt-key add - &&\
-	sh -c 'echo "deb http://apt.newrelic.com/debian/ newrelic non-free" > /etc/apt/sources.list.d/newrelic.list' &&\
-	apt-get update &&\
-	DEBIAN_FRONTEND=noninteractive apt-get install -y newrelic-php5)
-	
-#newrelic-install install
-
-# disable daemonization
-RUN sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php5/fpm/php-fpm.conf
+RUN (apt-get update &&\
+  apt-get install -y wget &&\
+  wget -O - http://download.newrelic.com/548C16BF.gpg | apt-key add - &&\
+  sh -c 'echo "deb http://apt.newrelic.com/debian/ newrelic non-free" > /etc/apt/sources.list.d/newrelic.list' &&\
+  apt-get update &&\
+  DEBIAN_FRONTEND=noninteractive apt-get install -y php5-curl php5-gd php5-fpm php5-imagick php5-mcrypt \
+    php5-memcache php5-memcached php5-mysql dnsutils imagemagick ssmtp whois newrelic-php5 &&\
+  sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php5/fpm/php-fpm.conf)
 
 # add run script
 ADD run.sh /usr/local/bin/run
 
-#VOLUME ["/run"]
 ENTRYPOINT ["/usr/local/bin/run"]
 CMD ["/usr/sbin/php5-fpm","-R","--fpm-config","/etc/php5/fpm/php-fpm.conf"]
--- run.sh
+++ run.sh
@@ -6,42 +6,42 @@ LICENSE_KEY=${LICENSE_KEY:-}
 
 if [ ! -f /tmp/configured ]
 then
-	if [ ! ${MAX_SIZE} = "8" ]
-	then
-		echo "Setting 'post_max_size' and 'upload_max_filesize' to '${MAX_SIZE}'"
-		sed -i "s/post_max_size = 8M/post_max_size = ${MAX_SIZE}M/g" /etc/php5/fpm/php.ini
-		sed -i "s/upload_max_filesize = 2M/upload_max_filesize = ${MAX_SIZE}M/g" /etc/php5/fpm/php.ini
-	else
-		echo "Using default value '${MAX_SIZE}' for 'post_max_size' and 'upload_max_filesize'"
-	fi
+  if [ ! ${MAX_SIZE} = "8" ]
+  then
+    echo "Setting 'post_max_size' and 'upload_max_filesize' to '${MAX_SIZE}'"
+    sed -i "s/post_max_size = 8M/post_max_size = ${MAX_SIZE}M/g" /etc/php5/fpm/php.ini
+    sed -i "s/upload_max_filesize = 2M/upload_max_filesize = ${MAX_SIZE}M/g" /etc/php5/fpm/php.ini
+  else
+    echo "Using default value '${MAX_SIZE}' for 'post_max_size' and 'upload_max_filesize'"
+  fi
 
-	if [ ! ${MAX_CHILDREN} = "5" ]
-	then
-		echo "Setting 'max_children' to '${MAX_CHILDREN}'"
-		sed -i "s/pm.max_children = 5/pm.max_children = ${MAX_CHILDREN}/g" /etc/php5/fpm/pool.d/www.conf
-	else
-		echo "Using default value '${MAX_CHILDREN}' for 'max_children'"
-	fi
+  if [ ! ${MAX_CHILDREN} = "5" ]
+  then
+    echo "Setting 'max_children' to '${MAX_CHILDREN}'"
+    sed -i "s/pm.max_children = 5/pm.max_children = ${MAX_CHILDREN}/g" /etc/php5/fpm/pool.d/www.conf
+  else
+    echo "Using default value '${MAX_CHILDREN}' for 'max_children'"
+  fi
 
-	if [ ${LISTEN} = "port" ]
-	then
-		echo "Disabling UNIX socket; enabling listening on TCP port 9000"
-		sed -i "s/listen = \/var\/run\/php5-fpm.sock/listen = 9000/g" /etc/php5/fpm/pool.d/www.conf
-	else
-		echo "Using default value '/var/run/php5-fpm.sock' for 'listen'"
-	fi
+  if [ ${LISTEN} = "port" ]
+  then
+    echo "Disabling UNIX socket; enabling listening on TCP port 9000"
+    sed -i "s/listen = \/var\/run\/php5-fpm.sock/listen = 9000/g" /etc/php5/fpm/pool.d/www.conf
+  else
+    echo "Using default value '/var/run/php5-fpm.sock' for 'listen'"
+  fi
 
-	if [ ! -z ${LICENSE_KEY} ]
-	then
-		echo "Setting New Relic license key to '${LICENSE_KEY}'"
-		sed -i "s/;newrelic.enabled = true/newrelic.enabled = true/g" /etc/php5/mods-available/newrelic.ini
-		echo "Enabling the New Relic PHP agent"
-		sed -i "s/newrelic.license = ""/newrelic.license = "${LICENSE_KEY}"/g" /etc/php5/mods-available/newrelic.ini
-	else
-		echo "Skipping New Relic setup"
-	fi
-	touch /tmp/configured
-	echo "Configuration complete."
+  if [ ! -z ${LICENSE_KEY} ]
+  then
+    echo "Setting New Relic license key to '${LICENSE_KEY}'"
+    sed -i "s/;newrelic.enabled = true/newrelic.enabled = true/g" /etc/php5/mods-available/newrelic.ini
+    echo "Enabling the New Relic PHP agent"
+    sed -i "s/newrelic.license = ""/newrelic.license = "${LICENSE_KEY}"/g" /etc/php5/mods-available/newrelic.ini
+  else
+    echo "Skipping New Relic setup"
+  fi
+  touch /tmp/configured
+  echo "Configuration complete."
 fi
 
 exec "$@"
